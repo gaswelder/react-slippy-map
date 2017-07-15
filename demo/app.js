e4a1cc970d4a3306966d8a009f865266ca40263d
gas <gasweld@gmail.com>
2017-07-16 02:40:03 +0300

    Demo fix

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {SlippyMapWithControls, pinned, Clusters} from '..';
+import {SlippyMapWithControls, pinned, Clusters} from '../src';
 
 const drivers = [
 	{id: '2606', coords: {latitude: 53.938787, longitude: 27.584183}},
