ab9c3a03efc91be9c7827728cfda53774ee698c7
gasweld <gasweld@gmail.com>
2019-05-06 21:15:21 +0300

    Library upgrades

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-	entry: './app.js',
-	output: {
-		path: __dirname,
-		filename: 'demo.bin.js'
-	},
-	module: {
-		rules: [
-			{test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']}
-		]
-	}
+  entry: "./app.js",
+  output: {
+    path: __dirname,
+    filename: "demo.bin.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/react", "@babel/env"] }
+        }
+      }
+    ]
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: __dirname,
-		filename: 'index.js',
-		libraryTarget: 'umd'
-	},
-	module: {
-		rules: [
-			{test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']}
-		]
-	},
-	externals: ['react', 'react-dom']
+  entry: "./src/index.js",
+  output: {
+    path: __dirname,
+    filename: "index.js",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/env", "@babel/react"] }
+        }
+      }
+    ]
+  },
+  externals: ["react", "react-dom"]
 };
