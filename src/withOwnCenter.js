15d8e9c03be007af3d7819e59aa10fcfb252df6b
gasweld <gasweld@gmail.com>
2022-10-01 02:49:08 +0400

    Fix defaultCenter prop

--- src/withOwnCenter.js
+++ src/withOwnCenter.js
@@ -4,7 +4,7 @@ const defaultCenter = { latitude: 53.9049, longitude: 27.5609 };
 
 export default function withOwnCenter(M) {
   return (props) => {
-    const [center, setCenter] = useState(defaultCenter);
+    const [center, setCenter] = useState(props.defaultCenter || defaultCenter);
 
     const onAreaChange = props.onAreaChange;
     const handleCenterChange = useCallback(
