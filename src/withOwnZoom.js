1e76a3665a6919ca6aadfdb9bf8ca9f5707e690a
gasweld <gasweld@gmail.com>
2022-10-02 22:24:33 +0400

    Update syntax

--- src/withOwnZoom.js
+++ src/withOwnZoom.js
@@ -1,93 +1,75 @@
-import React from "react";
+import React, { useState, useCallback, useRef } from "react";
 
 const containerStyle = {
   position: "relative",
-  height: "100%"
+  height: "100%",
 };
 
 const zoomStyle = {
   position: "absolute",
   right: "10px",
-  bottom: "10px"
+  bottom: "10px",
 };
 
-function ZoomControl(props) {
-  const { value, min, max, step, onChange } = props;
-
-  const handleChange = e => {
-    onChange(e.target.value);
-  };
-
-  return (
-    <div style={zoomStyle}>
-      ({value})
-      <input
-        type="range"
-        min={min}
-        max={max}
-        step={step}
-        value={value}
-        onChange={handleChange}
-      />
-    </div>
-  );
-}
+const clamp = (min, max, val) => {
+  let r = val;
+  if (r > max) r = max;
+  if (r < min) r = min;
+  return r;
+};
 
 export default function withOwnZoom(M) {
-  class WithOwnZoom extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        zoom: props.defaultZoom
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.onWheel = this.onWheel.bind(this);
-    }
+  return ({
+    minZoom = 2,
+    maxZoom = 20,
+    zoomStep = 0.1,
+    defaultZoom = 16,
+    ...otherProps
+  }) => {
+    const [zoom, setZoom] = useState(defaultZoom);
+    const ignoreWheelUntil = useRef(0);
 
-    handleChange(val) {
-      const { minZoom, maxZoom } = this.props;
+    const $handleChange = useCallback(
+      (val) => {
+        setZoom(clamp(minZoom, maxZoom, Math.round(val * 100) / 100));
+      },
+      [minZoom, maxZoom]
+    );
 
-      const fixRounding = num => Math.round(num * 100) / 100;
-      const zoom = Math.min(maxZoom, Math.max(minZoom, fixRounding(val)));
-      this.setState({ zoom });
-    }
+    const $onWheel = useCallback(
+      (event) => {
+        // Throttle barrier
+        if (
+          ignoreWheelUntil.current &&
+          event.timeStamp < ignoreWheelUntil.current
+        ) {
+          return;
+        }
+        let delay = 33 + zoomStep * 166;
+        if (delay > 200) delay = 200;
+        ignoreWheelUntil.current = event.timeStamp + delay;
+        $handleChange(event.deltaY > 0 ? zoom - zoomStep : zoom + zoomStep);
+      },
+      [ignoreWheelUntil.current, zoom, zoomStep]
+    );
 
-    onWheel(event) {
-      // Throttle barrier
-      if (this.ignoreWheelUntil && event.timeStamp < this.ignoreWheelUntil) {
-        return;
-      }
-
-      const { zoomStep } = this.props;
-      const { zoom } = this.state;
-
-      let delay = 33 + this.props.zoomStep * 166;
-      if (delay > 200) delay = 200;
-      this.ignoreWheelUntil = event.timeStamp + delay;
-
-      this.handleChange(event.deltaY > 0 ? zoom - zoomStep : zoom + zoomStep);
-    }
-
-    render() {
-      return (
-        <div style={containerStyle}>
-          <M zoom={this.state.zoom} onWheel={this.onWheel} {...this.props} />
-          <ZoomControl
-            onChange={this.handleChange}
-            min={this.props.minZoom}
-            max={this.props.maxZoom}
-            value={this.state.zoom}
-            step={this.props.zoomStep}
+    return (
+      <div style={containerStyle}>
+        <M zoom={zoom} onWheel={$onWheel} {...otherProps} />
+        <div style={zoomStyle}>
+          ({zoom})
+          <input
+            type="range"
+            min={minZoom}
+            max={maxZoom}
+            step={zoomStep}
+            value={zoom}
+            onChange={(e) => {
+              $handleChange(e.target.value);
+            }}
           />
         </div>
-      );
-    }
-  }
-  WithOwnZoom.defaultProps = {
-    defaultZoom: 16,
-    minZoom: 0,
-    maxZoom: 20,
-    zoomStep: 0.1
+      </div>
+    );
   };
-  return WithOwnZoom;
 }
